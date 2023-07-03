1341528ef0a356f18e4f5270769833fb6653c825
Azraei Yusof <mazei513@gmail.com>
2023-07-03 21:15:17 +0800

    Construct 15 iterations at a time

--- main.go
+++ main.go
@@ -13,17 +13,9 @@ func main() {
 			out(<-ch)
 		}
 	}()
-	for i := 0; i < math.MaxInt; i++ {
-		str := ""
-		if i%3 == 0 {
-			str += "Fizz"
-		}
-		if i%5 == 0 {
-			str += "Buzz"
-		}
-		if str == "" {
-			str = fmt.Sprint(i)
-		}
+	for i := 1; i < math.MaxInt; i += 15 {
+		str := fmt.Sprintf("%d\n%d\nFizz\n%d\nBuzz\nFizz\n%d\n%d\nFizz\nBuzz\n%d\nFizz\n%d\n%d\nFizzBuzz\n",
+			i, i+1, i+3, i+6, i+7, i+10, i+12, i+13)
 		ch <- str
 	}
 }
