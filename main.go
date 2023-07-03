07609c04ddb5371a1e8ba877e4e872ab6abd988f
Azraei Yusof <mazei513@gmail.com>
2023-07-03 21:22:02 +0800

    Removed string concat, fully rely on strings.Builder

--- main.go
+++ main.go
@@ -11,21 +11,29 @@ func main() {
 	sb := &strings.Builder{}
 	sb.Grow(1000000)
 	for i := 1; i < math.MaxInt; i += 15 {
-		sb.WriteString(strconv.Itoa(i) + "\n" +
-			strconv.Itoa(i+1) + "\n" +
-			"Fizz\n" +
-			strconv.Itoa(i+3) + "\n" +
-			"Buzz\n" +
-			"Fizz\n" +
-			strconv.Itoa(i+6) + "\n" +
-			strconv.Itoa(i+7) + "\n" +
-			"Fizz\n" +
-			"Buzz\n" +
-			strconv.Itoa(i+10) + "\n" +
-			"Fizz\n" +
-			strconv.Itoa(i+12) + "\n" +
-			strconv.Itoa(i+13) + "\n" +
-			"FizzBuzz\n")
+		sb.WriteString(strconv.Itoa(i))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa(i + 1))
+		sb.WriteString("\n")
+		sb.WriteString("Fizz\n")
+		sb.WriteString(strconv.Itoa(i + 3))
+		sb.WriteString("\n")
+		sb.WriteString("Buzz\n")
+		sb.WriteString("Fizz\n")
+		sb.WriteString(strconv.Itoa(i + 6))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa(i + 7))
+		sb.WriteString("\n")
+		sb.WriteString("Fizz\n")
+		sb.WriteString("Buzz\n")
+		sb.WriteString(strconv.Itoa(i + 10))
+		sb.WriteString("\n")
+		sb.WriteString("Fizz\n")
+		sb.WriteString(strconv.Itoa(i + 12))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa(i + 13))
+		sb.WriteString("\n")
+		sb.WriteString("FizzBuzz\n")
 		if sb.Len() > 500000 {
 			os.Stdout.WriteString(sb.String())
 			sb.Reset()
