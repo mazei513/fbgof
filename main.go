c99b0369f01083779a29317f2840bb17017db824
Azraei Yusof <mazei513@gmail.com>
2023-07-03 21:20:25 +0800

    Abandon concurrency, use strings.Builder for buffer

--- main.go
+++ main.go
@@ -1,45 +1,34 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	buf := &bytes.Buffer{}
-	ch := make(chan []byte, 50)
-	go func() {
-		for {
-			buf.Write(<-ch)
-			if buf.Len() < 1000000 {
-				continue
-			}
-			io.Copy(os.Stdout, buf)
-			buf.Reset()
-		}
-	}()
+	sb := &strings.Builder{}
+	sb.Grow(1000000)
 	for i := 1; i < math.MaxInt; i += 15 {
-		ch <- s(i)
+		sb.WriteString(strconv.Itoa(i) + "\n" +
+			strconv.Itoa(i+1) + "\n" +
+			"Fizz\n" +
+			strconv.Itoa(i+3) + "\n" +
+			"Buzz\n" +
+			"Fizz\n" +
+			strconv.Itoa(i+6) + "\n" +
+			strconv.Itoa(i+7) + "\n" +
+			"Fizz\n" +
+			"Buzz\n" +
+			strconv.Itoa(i+10) + "\n" +
+			"Fizz\n" +
+			strconv.Itoa(i+12) + "\n" +
+			strconv.Itoa(i+13) + "\n" +
+			"FizzBuzz\n")
+		if sb.Len() > 500000 {
+			os.Stdout.WriteString(sb.String())
+			sb.Reset()
+		}
 	}
-}
-
-func s(i int) []byte {
-	return []byte(strconv.Itoa(i) + "\n" +
-		strconv.Itoa(i+1) + "\n" +
-		"Fizz\n" +
-		strconv.Itoa(i+3) + "\n" +
-		"Buzz\n" +
-		"Fizz\n" +
-		strconv.Itoa(i+6) + "\n" +
-		strconv.Itoa(i+7) + "\n" +
-		"Fizz\n" +
-		"Buzz\n" +
-		strconv.Itoa(i+10) + "\n" +
-		"Fizz\n" +
-		strconv.Itoa(i+12) + "\n" +
-		strconv.Itoa(i+13) + "\n" +
-		"FizzBuzz\n")
 }
